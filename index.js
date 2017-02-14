b89f2d1856ddedbf4f6d40787d0e43a2c0817e05
Petr Kovář <petr.kovar.1992@gmail.com>
2017-02-14 09:57:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,111 @@
+/*** FirebaseModule Z-Way HA module *******************************************
+
+ Version: 1.0.0
+ (c) Petr Kovář, 2017
+ -----------------------------------------------------------------------------
+ Author: Petr Kovář <kovarp15@fel.cvut.cz>
+ Description:
+ This module allows to send event notifications via Firebase
+ server https://github.com/kovarp15/FirebaseModule
+
+ ******************************************************************************/
+
+// ----------------------------------------------------------------------------
+// --- Class definition, inheritance and setup
+// ----------------------------------------------------------------------------
+
+
+function FirebaseModule (id, controller) {
+    // Call superconstructor first (AutomationModule)
+    FirebaseModule.super_.call(this, id, controller);
+
+}
+
+inherits(FirebaseModule, AutomationModule);
+
+_module = FirebaseModule;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+FirebaseModule.prototype.init = function (config) {
+    FirebaseModule.super_.prototype.init.call(this, config);
+
+    var self = this;
+
+    this.vDev = this.controller.devices.create({
+        deviceId: "Firebase_" + this.id,
+        defaults: {
+            deviceType: "toggleButton",
+            metrics: {
+                level: 'on', // it is always on, but usefull to allow bind
+                icon: '',
+                title: 'Firebase ' + this.id
+            }
+        },
+        overlay: {},
+        handler: function () {
+
+            // If API Key from mandrillapp.com and Email exist, then send email
+            //if (typeof self.config.api_key_email !== 'undefined' && typeof self.config.email !== 'undefined') {
+            if (self.config.api_key && self.config.devide_id) {
+                http.request({
+                    method: 'POST',
+                    url: "https://fcm.googleapis.com/fcm/send",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': self.config.api_key
+                    },
+                    to: self.config.device_id,
+                    data: {
+                        "hello":"Hello World"
+                    }
+                });
+            }
+
+            self.vDev.set("metrics:level", "on"); // update on ourself to allow catch this event
+        },
+        moduleId: this.id
+    });
+
+    /*self.vDev = this.controller.devices.create({
+        deviceId: "SecurityZone_"+this.id,
+        defaults: {
+            metrics: {
+                triggeredDevices: [],
+                level: 'off',
+                state: 'off',
+                title: self.langFile.m_title+' '+self.langFile['type_'+self.config.type],
+                icon: self.imagePath+"/icon.png"
+            }
+        },
+        overlay: {
+            metrics: {
+                securityType: self.type
+            },
+            probeType: 'controller_security',
+            deviceType: 'switchBinary'
+        },
+        handler: function(command, args) {
+            if (command !== 'on'
+                && command !== 'off') {
+                return;
+            }
+            self.changeState(command);
+        },
+        moduleId: this.id
+    });*/
+
+};
+
+
+FirebaseModule.prototype.stop = function () {
+
+    if (this.vDev) {
+        this.controller.devices.remove(this.vDev.id);
+        this.vDev = null;
+    }
+
+    FirebaseModule.super_.prototype.stop.call(this);
+};
